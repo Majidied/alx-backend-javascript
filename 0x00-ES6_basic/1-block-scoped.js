9576ad03ae04053e790b8613adefb6dc2480f95b
majidied <majidimajidi2003@gmail.com>
2024-05-29 13:31:53 +0100

    Refactor block-scoped variables in taskBlock function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    let task = false;
+    let task2 = true;
+
     if (trueOrFalse) {
         task = true;
-        let task2 = false;
+        task2 = false;
     }
 
     return [task, task2];
-
 }
