4676c9452c936b8d169c1ed4b671ae6c537b04a0
majidied <majidimajidi2003@gmail.com>
2024-05-30 02:11:03 +0100

    Add getSanFranciscoDescription function in 0x00-ES6_basic/6-string-interpolation.js

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,3 @@
+export default function getSanFranciscoDescription() {
+    return `As of 2017, it was the seventh-highest income county in the United States, with a per capita personal income of $(budget.income). As of 2015, San Francisco proper had a GDP of $(budget.gdp), and a GDP per capita of $(budget.capita).`;
+}
