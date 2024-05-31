08a102f390353b0c4ee839076125dd7c50228224
majidied <majidimajidi2003@gmail.com>
2024-05-31 01:36:48 +0100

    Fix appendToEachArrayValue function in 0x00-ES6_basic/10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
-    appendString + value;
+    let index = array.indexOf(value);
+    array[index] = appendString + value;
   }
 
   return array;
