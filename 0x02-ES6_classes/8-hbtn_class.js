75585089710ea7d5c39894d8723d67dd49f9398f
majidied <majidimajidi2003@gmail.com>
2024-06-10 20:35:15 +0100

    Fix formatting in 0x02-ES6_classes/8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -5,8 +5,8 @@ export default class HolbertonClass {
   }
 
   [Symbol.toPrimitive](hint) {
-    if (hint === "string") return this._location;
-    if (hint === "number") return this._size;
+    if (hint === 'string') return this._location;
+    if (hint === 'number') return this._size;
     return null;
   }
 }
