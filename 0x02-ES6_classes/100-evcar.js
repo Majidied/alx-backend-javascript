b6c973f501f18cc18614dc73c1c5508f90a968d5
majidied <majidimajidi2003@gmail.com>
2024-06-10 22:05:24 +0100

    Add EvCar class to 0x02-ES6_classes/100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car';
+
+export default class EvCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
