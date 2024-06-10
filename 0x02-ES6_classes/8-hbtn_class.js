b3658790a6f09cb1136c8bc7acf28134014f36fd
majidied <majidimajidi2003@gmail.com>
2024-06-10 20:32:42 +0100

    Fix missing indentation in HolbertonClass constructor

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -3,6 +3,7 @@ export default class HolbertonClass {
     this._size = size;
     this._location = location;
   }
+  
   [Symbol.toPrimitive](hint) {
     if (hint === 'string') return this._location;
     if (hint === 'number') return this._size;
