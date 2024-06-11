307cce2d3976b8b08c8e5ae8773dbe922be791dc
majidied <majidimajidi2003@gmail.com>
2024-06-11 02:13:59 +0100

    Fix error handling in divideFunction in 0x01-ES6_promise/8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,9 @@
 export default function divideFunction(numerator, denominator) {
   return new Promise((resolve, reject) => {
-    try {
+    if (denominator === 0) {
+      reject(new Error('cannot divide by 0'));
+    } else {
       resolve(numerator / denominator);
-    } catch (error) {
-      reject(error);
     }
   });
 }
