7c54b01f9496706e230e6eadf6098974d6c851d3
majidied <majidimajidi2003@gmail.com>
2024-06-11 01:54:09 +0100

    Refactor loadBalancer function in 0x01-ES6_promise/7-load_balancer.js to use Promise.race instead of Promise.all

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.all([chinaDownload, USDownload]).then((values) => {
-    console.log(`China [${values[0].body}] response received`);
-    console.log(`US [${values[1].body}] response received`);
-  });
+  return Promise.race([chinaDownload, USDownload]).then((res) => res);
 }
