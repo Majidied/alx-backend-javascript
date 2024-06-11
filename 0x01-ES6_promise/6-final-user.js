55d6f427fac51fdf17f62a2592a93512da5d4b20
majidied <majidimajidi2003@gmail.com>
2024-06-11 01:02:42 +0100

    Refactor handleProfileSignup function in 0x01-ES6_promise/3-all.js and add error handling for signup system offline

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,18 @@
-import signUpUser from './4-user-promise';
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
+  const signUp = signUpUser(firstName, lastName)
     .then((user) => {
       console.log(`${user.firstName} ${user.lastName}`);
+      return { firstName: user.firstName, lastName: user.lastName };
     })
-    .catch(() => console.log('Signup system offline'))
-    .finally(() => console.log(`Upload of ${fileName} complete`));
+    .catch(() => {
+      console.log("Signup system offline");
+      throw new Error("Signup system offline");
+    });
+
+  const upload = new Promise((resolve, reject) => {
+    console.log(`Upload of ${fileName} complete`);
+    reject(new Error(`${fileName} cannot be processed`));
+  });
+
+  return Promise.allSettled([signUp, upload]);
 }
