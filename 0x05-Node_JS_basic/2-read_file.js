b6cda8657ff486ca283c2814fc3b94a6e1db9637
majidied <majidimajidi2003@gmail.com>
2024-07-24 21:43:51 +0100

    Refactor read_file.js to count and display the number of students in CS and SWE

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    console.log(data);
+    console.log(`Number of students: ${data.split('\n').length - 1}`);
+    console.log(`Number of students in CS: ${data.split('\n').filter((line) => line.split(',')[3] === 'CS').length - 1}. List: ${data.split('\n').filter((line) => line.split(',')[3] === 'CS').map((line) => line.split(',')[0]).slice(1).join(', ')}`);
+    console.log(`Number of students in SWE: ${data.split('\n').filter((line) => line.split(',')[3] === 'SWE').length - 1}. List: ${data.split('\n').filter((line) => line.split(',')[3] === 'SWE').map((line) => line.split(',')[0]).slice(1).join(', ')}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
