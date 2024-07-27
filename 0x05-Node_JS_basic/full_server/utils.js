cd85b06472d1040ac8627f454e21299a88659c3b
majidied <majidimajidi2003@gmail.com>
2024-07-27 11:02:11 +0100

    Refactor code to improve student counting and display

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -3,50 +3,45 @@ import fs from 'fs';
 /**
  * Reads the data of students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Bezaleel Olakunori
  * @returns {Promise<{
  *   String: {firstname: String, lastname: String, age: number}[]
  * }>}
  */
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
-    if (!dataPath) {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+    if (err) {
       reject(new Error('Cannot load the database'));
     }
-    if (dataPath) {
-      fs.readFile(dataPath, (err, data) => {
-        if (err) {
-          reject(new Error('Cannot load the database'));
+    if (data) {
+      const fileLines = data.toString('utf-8').trim().split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0,
+          studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
         }
-        if (data) {
-          const fileLines = data.toString('utf-8').trim().split('\n');
-          const studentGroups = {};
-          const dbFieldNames = fileLines[0].split(',');
-          const studentPropNames = dbFieldNames.slice(
-            0,
-            dbFieldNames.length - 1,
-          );
-          for (const line of fileLines.slice(1)) {
-            const studentRecord = line.split(',');
-            const studentPropValues = studentRecord.slice(
-              0,
-              studentRecord.length - 1,
-            );
-            const field = studentRecord[studentRecord.length - 1];
-            if (!Object.keys(studentGroups).includes(field)) {
-              studentGroups[field] = [];
-            }
-            studentGroups[field].push(
-              studentPropNames.reduce((acc, propName, idx) => {
-                acc[propName] = studentPropValues[idx];
-                return acc;
-              }, {}),
-            );
-          }
-          resolve(studentGroups);
-        }
-      });
+        studentGroups[field].push(
+          studentPropNames.reduce((acc, propName, idx) => {
+            acc[propName] = studentPropValues[idx];
+            return acc;
+          }, {})
+        );
+      }
+      resolve(studentGroups);
     }
   });
+}
+});
 
 export default readDatabase;
 module.exports = readDatabase;
