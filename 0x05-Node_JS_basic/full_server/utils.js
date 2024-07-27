e0f1a417b0d798d0a702ffd99e88e455fda7bc79
majidied <majidimajidi2003@gmail.com>
2024-07-27 10:51:11 +0100

    Refactor code to improve student counting and display

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -8,8 +8,7 @@ import fs from 'fs';
  *   String: {firstname: String, lastname: String, age: number}[]
  * }>}
  */
-const readDatabase = (dataPath) =>
-  new Promise((resolve, reject) => {
+const readDatabase = (dataPath) => new Promise((resolve, reject) => {
     if (!dataPath) {
       reject(new Error('Cannot load the database'));
     }
@@ -24,14 +23,14 @@ const readDatabase = (dataPath) =>
           const dbFieldNames = fileLines[0].split(',');
           const studentPropNames = dbFieldNames.slice(
             0,
-            dbFieldNames.length - 1
+            dbFieldNames.length - 1,
           );
 
           for (const line of fileLines.slice(1)) {
             const studentRecord = line.split(',');
             const studentPropValues = studentRecord.slice(
               0,
-              studentRecord.length - 1
+              studentRecord.length - 1,
             );
             const field = studentRecord[studentRecord.length - 1];
             if (!Object.keys(studentGroups).includes(field)) {
